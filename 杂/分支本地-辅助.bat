e8133f8d5c0bde23bb5459bc3ed85ecd22538067
W_Cloud <yunkezengren@outlook.com>
2026-01-26 21:57:20 +0800

    00

--- 杂/分支本地-辅助.bat
+++ 杂/分支本地-辅助.bat
@@ -56,46 +56,43 @@ rebuild -j8
 rebuild -j8 && bin\blender
 
 
-
-
-
 cd /d E:/blender-git/blender
-set Branch=change-socket-shape
-set Branch=bevelnode
-set Branch=test-branch
+@REM set Branch=change-socket-shape
+@REM set Branch=bevelnode
+@REM set Branch=test-branch
 set Branch=blender
 
-# set nj_Branch=E:\blender-git\%Branch%_ninja_lite
-# make nobuild ninja lite builddir %nj_Branch%
-# cd %nj_Branch%
+@REM set nj_Branch=E:\blender-git\%Branch%_ninja_lite
+@REM make nobuild ninja lite builddir %nj_Branch%
+@REM cd %nj_Branch%
 
-# set nj_Branch=E:\blender-git\%Branch%_ninja_lite_debug
-# make nobuild ninja lite debug builddir %nj_Branch%
-# cd %nj_Branch%
+set nj_Branch=E:\blender-git\%Branch%_ninja_lite_debug
+make nobuild ninja lite debug builddir %nj_Branch%
+cd %nj_Branch%
 
-# set nj_Branch=E:\blender-git\%Branch%_ninja_debug
-# make nobuild ninja debug builddir %nj_Branch%
-# cd %nj_Branch%
+@REM set nj_Branch=E:\blender-git\%Branch%_ninja_debug
+@REM make nobuild ninja debug builddir %nj_Branch%
+@REM cd %nj_Branch%
 
-# set nj_Branch=E:\blender-git\%Branch%_ninja_debug_developer
-# make nobuild ninja debug developer builddir %nj_Branch%
-# cd %nj_Branch%
+@REM set nj_Branch=E:\blender-git\%Branch%_ninja_debug_developer
+@REM make nobuild ninja debug developer builddir %nj_Branch%
+@REM cd %nj_Branch%
 
-set nj_release_Branch=E:\blender-git\%Branch%_ninja_release
-make nobuild ninja builddir %nj_release_Branch%
-cd %nj_release_Branch%
+@REM set nj_release_Branch=E:\blender-git\%Branch%_ninja_release
+@REM make nobuild ninja builddir %nj_release_Branch%
+@REM cd %nj_release_Branch%
 
 code CmakeCache.txt
-#   可以按需要启用一些
-#   启用 CMAKE_EXPORT_COMPILE_COMMANDS: 会在构建目录中创建compile_commands.json
-#   启用 echo: CMAKE_VERBOSE_MAKEFILE:
-#  lite 还需要这三个
-#   启用 WITH_TBB
-#   启用 ime: WITH_INPUT_IME
-#   启用 i18n: WITH_INTERNATIONAL
+@REM   可以按需要启用一些
+@REM   启用 CMAKE_EXPORT_COMPILE_COMMANDS: 会在构建目录中创建compile_commands.json
+@REM   启用 echo: CMAKE_VERBOSE_MAKEFILE:
+@REM  lite 还需要这三个
+@REM   启用 WITH_TBB
+@REM   启用 ime: WITH_INPUT_IME
+@REM   启用 i18n: WITH_INTERNATIONAL
 
-#   此外: CMAKE_BUILD_TYPE 可以改, lite时是Release
+@REM   此外: CMAKE_BUILD_TYPE 可以改, lite时是Release
 rebuild -j8
-# 问的AI, 我是4核8线程, 所以 j8 会创建8个线程还是进程, 卡的话可以开少点
+@REM   问的AI, 我是4核8线程, 所以 j8 会创建8个线程还是进程, 卡的话可以开少点
 
 bin\blender
